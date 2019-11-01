aa0bd7c4287b6d28df09570035536fb32b7adf8e
Daniil <boiko.81@list.ru>
2019-11-01 23:23:15 +0300

    add array tasks and html-view for him

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,17 +2,45 @@
   .content-wrapper
     section
       .container
-        h1 Home
+        h1.ui-title-1 Home
+    section
+      .container
+        .task-list
+          .task-item(
+            v-for="task in tasks"
+            :key="task.id"
+            :class="{ completed: task.completed }"
+          )
+            .ui-card.ui-card--shadow
+              .task-item__info
+                span {{ task.whatWatch }}
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      tasks: [
+        {
+          'id': 1,
+          'title': 'GrowthBusters: Hooked on Growth',
+          'description': 'I directed this documentary challenge',
+          'whatWatch': 'film',
+          'completed': false,
+          'editing': false
+        },
+        {
+          'id': 2,
+          'title': 'Game of thrones',
+          'description': 'Best serials',
+          'whatWatch': 'Serial',
+          'completed': false,
+          'editing': false
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-
-</style>
+<style lang="stylus" scoped></style>
