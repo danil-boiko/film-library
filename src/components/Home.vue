cd252bc74e96361360f3de8ad40c22927b773042
Daniil <boiko.81@list.ru>
2019-11-01 23:51:58 +0300

    add description and design for tasks

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,7 +13,20 @@
           )
             .ui-card.ui-card--shadow
               .task-item__info
-                span {{ task.whatWatch }}
+                .task-item__main-info 
+                  span.ui-label.ui-label--light {{ task.whatWatch }}
+                  span Total Time:
+                span.button-close
+              .task-item__content
+                .task-item__header
+                  .ui-checkbox-wrapper
+                    input.ui-checkbox(
+                      type="checkbox"
+                      v-model="task.completed"
+                    )
+                  span.ui-title-3 {{ task.title }}
+                .task-item__body
+                  p.ui-text-regular {{ task.description }}
 </template>
 
 <script>
@@ -24,7 +37,7 @@ export default {
         {
           'id': 1,
           'title': 'GrowthBusters: Hooked on Growth',
-          'description': 'I directed this documentary challenge',
+          'description': 'I directed this documentary challenging the myths linking growth with prosperity and fulfillment. The film explores how our attitudes toward economics, consumption',
           'whatWatch': 'film',
           'completed': false,
           'editing': false
@@ -43,4 +56,30 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.tak-item
+  margin-bottom 20px
+  &:last-child
+    margin-bottom 0
+
+.ui-label
+  margin-right 8px
+
+.task-item__info
+  display flex
+  align-items center
+  justify-content space-between
+  margin-bottom 20px
+  .button-close
+    width 20px
+    height @width
+
+.task-item__header
+  display flex
+  align-items center
+  margin-bottom 18px
+  .ui-checkbox-wrapper
+    margin-right 8px
+  .ui-title-3 
+    margin-bottom 0
+</style>
