8618f58e6f36e74ca40c7facd7c891e0c16d6431
Daniil <boiko.81@list.ru>
2019-11-02 12:32:49 +0300

    add component Task.vue and remove array tasks to Task.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -33,7 +33,7 @@
           label(
             for="radioSerial"
           ) Serial
-        .total-time 
+        .total-time
           .total-time__film(
             v-if="whatWatch === 'Film'"
           )
@@ -48,30 +48,6 @@
             span.tag-title Dogs
             span.button-close
 
-    section
-      .container
-        .task-list
-          .task-item(
-            v-for="task in tasks"
-            :key="task.id"
-            :class="{ completed: task.completed }"
-          )
-            .ui-card.ui-card--shadow
-              .task-item__info
-                .task-item__main-info
-                  span.ui-label.ui-label--light {{ task.whatWatch }}
-                  span Total Time:
-                span.button-close
-              .task-item__content
-                .task-item__header
-                  .ui-checkbox-wrapper
-                    input.ui-checkbox(
-                      type="checkbox"
-                      v-model="task.completed"
-                    )
-                  span.ui-title-3 {{ task.title }}
-                .task-item__body
-                  p.ui-text-regular {{ task.description }}
 </template>
 
 <script>
@@ -81,25 +57,7 @@ export default {
       taskTitle: '',
       taskDescription: '',
       whatWatch: 'Film',
-      taskId: 3,
-      tasks: [
-        {
-          'id': 1,
-          'title': 'GrowthBusters: Hooked on Growth',
-          'description': 'I directed this documentary challenging the myths linking growth with prosperity and fulfillment. The film explores how our attitudes toward economics, consumption',
-          'whatWatch': 'film',
-          'completed': false,
-          'editing': false
-        },
-        {
-          'id': 2,
-          'title': 'Game of thrones',
-          'description': 'Best serials',
-          'whatWatch': 'Serial',
-          'completed': false,
-          'editing': false
-        }
-      ]
+      taskId: 3
     }
   },
   methods: {
@@ -135,34 +93,5 @@ export default {
   label
     margin-right 20px
     &:last-child
-      margin-bottom 0
-
-.task-item
-  margin-bottom 20px
-  &:last-child
-    margin-bottom 0
-
-.total-time
-  margin-bottom 20px 
-
-.ui-label
-  margin-right 8px
-
-.task-item__info
-  display flex
-  align-items center
-  justify-content space-between
-  margin-bottom 20px
-  .button-close
-    width 20px
-    height @width
-
-.task-item__header
-  display flex
-  align-items center
-  margin-bottom 18px
-  .ui-checkbox-wrapper
-    margin-right 8px
-  .ui-title-3
-    margin-bottom 0
+      margin-right 0
 </style>
