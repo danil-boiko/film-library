510d5fa3a35fcb658dfca413ce4703a778688bd8
Daniil <boiko.81@list.ru>
2019-11-20 15:41:22 +0300

    install Vuelidate and add conditions for input-email

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -7,31 +7,43 @@
            h1.ui-title-2 Hello banner
           .auth__form
             span.ui-title-2 Registration
-              form
+            form
+              .form-item(:class="{ 'errorInput': $v.email.$error }")
                 input(
                   type="email"
                   placeholder="Email"
                   v-model="email"
+                  :class="{ error: $v.email.$error }"
+                  @change="$v.email.$touch()"
                 )
-                input(
-                  type="password"
-                  placeholder="Password"
-                  v-model="password"
-                )
-                input(
-                  type="password"
-                  placeholder="Repeat your password"
-                  v-model="repeatPassword"
-                )
+                .error(v-if="!$v.email.required") Field is required
+                .error(v-if="!$v.email.email") Email is not correct
+              input(
+                type="password"
+                placeholder="Password"
+                v-model="password"
+              )
+              input(
+                type="password"
+                placeholder="Repeat your password"
+                v-model="repeatPassword"
+              )
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators'
 export default {
   data () {
     return {
       email: '',
       password: '',
       repeatPassword: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
     }
   }
 }
@@ -43,4 +55,18 @@ export default {
 .auth__banner,
 .auth__form
   width: 50%
+
+.form-item
+  .error
+    display none
+    margin-bottom 8px
+    font-size 13.4px
+    color #fc5c65
+  &.errorInput
+    .error
+      display block
+
+input
+  &.error
+    border-color #fc5c65
 </style>
