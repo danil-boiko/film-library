2c5ab4daf6a0fcf82396bfc43426a7c87faf7c8f
Daniil <boiko.81@list.ru>
2019-11-20 15:13:23 +0300

    add form and styles for her

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -2,17 +2,45 @@
   .content-wrapper
     section
       .container
-        h1 Registration
+        .auth
+          .auth__banner
+           h1.ui-title-2 Hello banner
+          .auth__form
+            span.ui-title-2 Registration
+              form
+                input(
+                  type="email"
+                  placeholder="Email"
+                  v-model="email"
+                )
+                input(
+                  type="password"
+                  placeholder="Password"
+                  v-model="password"
+                )
+                input(
+                  type="password"
+                  placeholder="Repeat your password"
+                  v-model="repeatPassword"
+                )
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      email: '',
+      password: '',
+      repeatPassword: ''
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.auth
+  display flex
+.auth__banner,
+.auth__form
+  width: 50%
 </style>
