168a1cdf9b3cc303243330d0725b7ae182b3c6f7
Daniil <boiko.81@list.ru>
2019-11-22 14:46:18 +0300

    add button submit

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -7,7 +7,7 @@
            h1.ui-title-2 Hello banner
           .auth__form
             span.ui-title-2 Registration
-            form
+            form(@submit.prevent="onSubmit")
               .form-item(:class="{ 'errorInput': $v.email.$error }")
                 input(
                   type="email"
@@ -38,6 +38,18 @@
                   @change="$v.repeatPassword.$touch()"
                 )
                 .error(v-if="!$v.repeatPassword.sameAsPassword") Passwords must be identical.
+              .button-list
+                button.button.button-primary(
+                  type="submit"
+                  :disabled="submitStatus === 'PENDING'"
+                ) Registration
+              .button-list.button-list--info
+                p.typo__p(v-if="submitStatus === 'OK'") Thanks for your submission!
+                p.typo__p(v-if="submitStatus === 'ERROR'") Please fill the form correctly.
+                p.typo__p(v-if="submitStatus === 'PENDING'") Sending...
+              .button-list.button-list--info
+                span Do tou have account?
+                  router-link(to="/login")  Enter Here
 </template>
 
 <script>
@@ -47,7 +59,8 @@ export default {
     return {
       email: '',
       password: '',
-      repeatPassword: ''
+      repeatPassword: '',
+      submitStatus: null
     }
   },
   validations: {
@@ -62,28 +75,69 @@ export default {
     repeatPassword: {
       sameAsPassword: sameAs('password')
     }
+  },
+  methods: {
+    onSubmit () {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+      } else {
+        console.log('submit!')
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+        // do your submit logic here
+        this.submitStatus = 'PENDING'
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+        }, 500)
+      }
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.auth
-  display flex
+.auth {
+  display: flex;
+}
+
 .auth__banner,
-.auth__form
-  width: 50%
+ .auth__form {
+  width: 50%;
+}
 
-.form-item
-  .error
-    display none
-    margin-bottom 8px
-    font-size 13.4px
-    color #fc5c65
-  &.errorInput
-    .error
-      display block
+.form-item {
+  .error {
+    display: none;
+    margin-bottom: 8px;
+    font-size: 13.4px;
+    color: #fc5c65;
+  }
 
-input
-  &.error
-    border-color #fc5c65
+  &.errorInput {
+    .error {
+      display: block;
+    }
+  }
+}
+
+input {
+  &.error {
+    border-color: #fc5c65;
+  }
+}
+
+.button-list
+  text-align right
+  margin-bottom 20px
+  &.button-list--info
+    text-align center
+    &:last-child
+      margin-bottom 0
+
+a
+  color #444ce0
 </style>
